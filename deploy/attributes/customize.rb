696356c90671f75af8c312870107dd042976b4fc
Swetha4b0 <m.swetha2@tcs.com>
2016-01-19 17:16:18 +0530

    Update customize.rb

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -52,7 +52,7 @@ node[:deploy].each do |application, deploy|
     raise "Invalid chef_provider '#{node[:deploy][application][:chef_provider]}' for app '#{application}'. Valid providers: #{valid_deploy_chef_providers.join(', ')}."
   end
   default[:deploy][application][:keep_releases] = node[:deploy][application][:keep_releases] ? node[:deploy][application][:keep_releases] : node[:opsworks][:deploy_keep_releases]
-  default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}/current"
+  default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}"
   default[:deploy][application][:document_root] = ''
   default[:deploy][application][:ignore_bundler_groups] = node[:opsworks][:rails][:ignore_bundler_groups]
   if deploy[:document_root]
